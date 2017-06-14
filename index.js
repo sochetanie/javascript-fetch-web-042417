55eafd0b8be7bd1138e0da53fa343b5fc449669f
Alla Gorik <allagorik2901@gmail.com>
2017-06-14 20:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much.";
+
+// with XMLHttpRequest
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Booo');
+};
+
+xhr.send();
+
+
+
+
+// with fetch
+const token = 'ca7bbbf42ccf2a4e46a1e86adff5b6000031fa22'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
